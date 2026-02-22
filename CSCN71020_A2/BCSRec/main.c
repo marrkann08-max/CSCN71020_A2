1ee3052b371434b03cd1e6b7421e8f3a953668ac
Keshav Markan <marrkann08@gmail.com>
2026-02-22 10:15:56 -0800

    Fixed getPerimeter function and all tests passing

--- CSCN71020_A2/BCSRec/main.c
+++ CSCN71020_A2/BCSRec/main.c
@@ -77,13 +77,13 @@ void setLength(int input, int *length) {
 }
 
 void setWidth(int input, int *width) {
-	if (input > 0 && input <= 100) {
+	if (input > 0 && input < 100) {
 		*width = input;
 	}
 }
 
 int getPerimeter(int *length, int *width) {
-	int perimeter = *length + *length + *width;
+	int perimeter = *length + *length + *width + *width;
 	return perimeter;
 }
 
